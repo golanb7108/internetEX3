70adff196c2992a1d6469edea53376999d11ce87
golanb7108 <golan3873@gmail.com>
2014-12-23 10:45:00 +0200

    breaked :(

--- hujinet.js
+++ hujinet.js
@@ -15,6 +15,7 @@ var socket;
 function create_http_response(http_req){
     var http_res = new httpresponse.HttpResponse();
     var file;
+
     var url_pathname = url.parse(http_req.url).pathname;
     var type = url_pathname.substr(url_pathname.lastIndexOf("."));
 
@@ -41,7 +42,12 @@ function create_http_response(http_req){
                 http_res.status_code = "200";
                 http_res.reason_phrase = "OK";
         }
-        socket.write(hujiparser.stringify(http_res));
+        socket.write(hujiparser.stringify(http_res), function(err){
+            if (err.code == 'ECONNRESET'){
+                console.log()
+
+            }
+        });
 
         if ((http_req.method == "GET") || (http_req.method == "POST")){
             var file_name = '\/' + root + url.parse(http_req.url).pathname
@@ -68,17 +74,20 @@ function create_http_response(http_req){
 
 
 exports.getServer = function(port, rootFolder){
-    var item;
     root = rootFolder;
     var server = net.createServer(function(in_socket) { //'connection' listener
+
         console.log('server connected');
         socket = in_socket;
         socket.on('data', function(data) {
             console.log('Data was received');
-            //var req_list = hujiparser.parse(data.toString());
-            //for (item in req_list) {create_http_response(item)}
-            var http_req = hujiparser.parse(data.toString()); //TODO - check the output is valid, if not - send an indicator resp
-            create_http_response(http_req);
+            var req_list = hujiparser.parse(data.toString());
+            for (var i = 0; i < req_list.length ; i++){
+                req_list[i].print();
+                create_http_response(req_list[i]);
+            }
+            //var http_req = hujiparser.parse(data.toString()); //TODO - check the output is valid, if not - send an indicator resp
+            //create_http_response(http_req);
 
         });
 
@@ -97,8 +106,9 @@ exports.getServer = function(port, rootFolder){
                 });
             }, 1000);
         }
+        console.log(e.code);
     });
-
+    server.maxConnections = 1024;
     server.listen(port, function() { //'listening' listener
         console.log('server bound');
     });
