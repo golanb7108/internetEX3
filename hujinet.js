7404475269774d7553bbda4b22d4a9237a0d50d2
golanb7108 <golan3873@gmail.com>
2014-12-23 11:48:36 +0200

    works fine - after adding timeout to the socket

--- hujinet.js
+++ hujinet.js
@@ -12,7 +12,17 @@ var root;
 var socket;
 
 
-function create_http_response(http_req){
+function writeResp(response, socket , close_socket ){
+    socket.write(response.toString(),'binary');
+    socket.on('drain', function(){
+        if (close_socket){
+            socket.end();
+        }
+    });
+
+}
+
+function create_http_response(http_req, socket){
     var http_res = new httpresponse.HttpResponse();
     var file;
 
@@ -32,8 +42,8 @@ function create_http_response(http_req){
             http_res.general_headers["Connection"] = "close";
             http_res.message_body = "The requested URL " + file + " was not found on this server"
             console.log("no file: " + err.message);
-
-            socket.write(hujiparser.stringify(http_res));
+            writeResp(hujiparser.stringify(http_res), socket, http_res.general_headers["Connection"] == "close");
+            //socket.write(hujiparser.stringify(http_res));
             return;
         }
         http_res.entity_headers["Content-Length"] = stats.size;
@@ -42,53 +52,50 @@ function create_http_response(http_req){
                 http_res.status_code = "200";
                 http_res.reason_phrase = "OK";
         }
-        socket.write(hujiparser.stringify(http_res), function(err){
-            if (err.code == 'ECONNRESET'){
-                console.log()
+        writeResp(hujiparser.stringify(http_res), socket, http_res.general_headers["Connection"] == "close");
 
+
+        //send the file it self
+        var file_name = '\/' + root + url.parse(http_req.url).pathname
+        fs.exists(file_name, function(exists){
+            if (exists){
+                var file_stream = fs.createReadStream(file_name);
+                file_stream.pipe(socket);
             }
-        });
 
-        if ((http_req.method == "GET") || (http_req.method == "POST")){
-            var file_name = '\/' + root + url.parse(http_req.url).pathname
-            fs.exists(file_name, function(exists){
-                if (exists){
-                    var file_stream = fs.createReadStream(file_name);
-                    file_stream.pipe(socket);
-                }
-
-                file_stream.on('error', function (err){
-                    http_res.status_code = "404";
-                    http_res.reason_phrase = "Not found";
-                    http_res.entity_headers["Content-Type"] = "text/plain";
-                    http_res.general_headers["Connection"] = "close";
-                    http_res.message_body = "The requested URL " + file_name + " was not found on this server"
-                    console.log("error message: " + err.message);
-                    socket.write(hujiparser.stringify(http_res));
-                    return;
-                });
+            file_stream.on('error', function (err){
+                http_res.status_code = "404";
+                http_res.reason_phrase = "Not found";
+                http_res.entity_headers["Content-Type"] = "text/plain";
+                http_res.general_headers["Connection"] = "close";
+                http_res.message_body = "The requested URL " + file_name + " was not found on this server"
+                console.log("error message: " + err.message);
+                writeResp(hujiparser.stringify(http_res), socket, http_res.general_headers["Connection"] == "close");
+//                socket.write(hujiparser.stringify(http_res));
+                return;
             });
-        }
+        });
     });
 }
 
 
 exports.getServer = function(port, rootFolder){
     root = rootFolder;
-    var server = net.createServer(function(in_socket) { //'connection' listener
-
+    var server = net.createServer(function(socket) { //'connection' listener
+        socket.setTimeout(2000);
         console.log('server connected');
-        socket = in_socket;
         socket.on('data', function(data) {
             console.log('Data was received');
             var req_list = hujiparser.parse(data.toString());
             for (var i = 0; i < req_list.length ; i++){
                 req_list[i].print();
-                create_http_response(req_list[i]);
+                create_http_response(req_list[i], socket);
             }
-            //var http_req = hujiparser.parse(data.toString()); //TODO - check the output is valid, if not - send an indicator resp
-            //create_http_response(http_req);
+        });
 
+        socket.on('timeout', function(){
+            socket.end();
+            socket.destroy();
         });
 
         socket.on('end', function() {
@@ -98,6 +105,15 @@ exports.getServer = function(port, rootFolder){
 
     server.on('error', function (e) {
         if (e.code == 'EADDRINUSE') {
+            console.log('Address in use, retrying...');
+            setTimeout(function () {
+                server.close();
+                server.listen(port, function() { //'listening' listener
+                    console.log('server bound');
+                });
+            }, 1000);
+        }
+        if (e.code == 'ECONNRESET') {
             console.log('Address in use, retrying...');
             setTimeout(function () {
                 server.close();
