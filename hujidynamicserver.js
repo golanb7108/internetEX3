be81aa14bea7ef3dbd6263f479644456c4555bb5
golanb7108 <golan3873@gmail.com>
2015-01-01 19:20:08 +0200

    first ex4

---
+++ hujidynamicserver.js
@@ -0,0 +1,38 @@
+/**
+ * Created by gbenami on 1/1/2015.
+ */
+
+var path = require ('path');
+var fs =  require('fs');
+var net =  require('net');
+
+var hujidynamicserver = function ()
+{
+
+    var dynamic = function(){
+
+    };
+    dynamic.stop = function(){
+
+    };
+    dynamic.use = function(resource, requestHandler){
+
+    };
+    dynamic.get = function(resource, requestHandler){
+
+    };
+    dynamic.post = function(resource, requestHandler){
+
+    };
+    dynamic.delete = function(resource, requestHandler){
+
+    };
+    dynamic.put = function(resource, requestHandler){
+
+    };
+};
+module.exports = hujidynamicserver;
+
+
+
+
--- hujiwebserver.js
+++ hujiwebserver.js
@@ -4,21 +4,87 @@
 
 /* All requires */
 var hujinet = require('./hujinet');
+var hujidynamicserver = require('./hujidynamicserver');
+
+var hujiwebserver = function(){
+    return hujidynamicserver;
+};
 
 /* Servers list */
 var http_servers = [];
 var server_id = 0;
 
 /* Start a new server and return its id */
-exports.start = function (port, rootFolder, callback){
+hujiwebserver.start = function (port, rootFolder, callback){
     http_servers[server_id] = hujinet.getServer(port, rootFolder, callback);
     http_servers[server_id].on('error', callback);
     return server_id++;
 };
 
-/* Close a server by its id */
+hujiwebserver.static = function(rootFolder)
+{
+
+};
+
+hujiwebserver.myUse = function()
+{
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Close a server by its id
 exports.stop = function (server_id, callback){
     http_servers[server_id].close();
     http_servers[server_id].on('error', callback);
     console.log("Server closed");
-};
+};*/
