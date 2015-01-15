ac985b3a5c74344da0bbfa401904a8113535f5b1
amitabel <amitabel1@gmail.com>
2015-01-15 13:31:47 +0200

    register and login

--- main.js
+++ main.js
@@ -2,15 +2,61 @@
  * Created by Amit Abel and Golan Ben Ami
  */
 
+var settings = require('./settings');
+var todoitems = require('./todoitems');
+var users = require('./users');
 var uuid = require('node-uuid');
+var hujiserver = require('./hujiwebserver');
 
-exports.username_already_exist_error = new Error("The user name is already in use. please enter a different user name");
+/* Server variables */
+port = 8124;
 
-var users = [];
-var todo_items = [];
+var app =hujiserver.start(port,function(e) {
+    e ? (console.log(e)) : console.log('server is up port 888');
+});
 
-function add_user(user_name, full_name, password){
-    if (user_already_exist(user_name)){
-        return setting.
+function register(request, response, next){
+    var session_id,
+        user_name;
+    try {
+        if (request.body_params === undefined){
+            throw settings.bad_request_format_error;
+        }
+        session_id = uuid.v1();
+        user_name = request.body_params['user_name'];
+        users.add_user(user_name,request.body_params['full_name'],request.body_params['password'],
+                request.body_params['verify_password'],session_id);
+        todoitems.new_user_todo_list(user_name);
+        response.cookie('user_name', user_name, {'expires':users.get_user(user_name).time_to_expire});
+        response.cookie('sessionId', session_id, {'expires':users.get_user(user_name).time_to_expire});
+        response.send(200, settings.STATUS_PHRASES[200]);
+    } catch (e) {
+        response.send(500, e.message);
     }
 }
+
+function login(request, response, next){
+    var password,
+        session_id,
+        user_name,
+        user_obj;
+    try {
+        if (request.body_params === undefined){
+            throw settings.bad_request_format_error;
+        }
+        user_name = request.body_params['user_name'];
+        password = request.body_params['password'];
+        session_id = uuid.v1();
+        if (users.try_to_register(user_name, password, session_id)){
+            response.cookie('user_name', user_name, {'expires':users.get_user(user_name).time_to_expire});
+            response.cookie('sessionId', session_id, {'expires':users.get_user(user_name).time_to_expire});
+            response.send(200, settings.STATUS_PHRASES[200]);
+        }
+    } catch (e) {
+        response.send(500, e.message);
+    }
+}
+
+app.post('/register', register);
+app.post('/login', login);
+
---
+++ todoitems.js
@@ -0,0 +1,11 @@
+/**
+ * Created by aabel on 15-Jan-15.
+ */
+
+var todo_items = [];
+
+function new_user_todo_list(user_name){
+
+}
+
+exports.new_user_todo_list = new_user_todo_list;
