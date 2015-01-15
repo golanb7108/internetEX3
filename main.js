ee60347c77ac764cf10e4e6f8b4c601251225d1d
amitabel <amitabel1@gmail.com>
2015-01-15 20:20:50 +0200

    add push, get and delete

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var uuid = require('node-uuid');
 var hujiserver = require('./hujiwebserver');
 
 /* Server variables */
-port = 8124;
+var port = 8124;
 
 hujiserver.start(port,function(e, server) {
     e?(console.log(e)):(console.log('server is up'));
@@ -20,6 +20,9 @@ hujiserver.start(port,function(e, server) {
         server.post('/login', login);
 
         server.get('/item', get_all_todo_items);
+        server.post('/item', add_item_to_todo_items);
+        server.put('/item', update_item_in_todo_items);
+        server.delete('/item', delete_item_in_todo_items);
     }
 });
 
@@ -40,7 +43,9 @@ function register(request, response, next){
         response.status(200).send(settings.STATUS_PHRASES[200]);
     } catch (e) {
         response.status(500).send(e.message);
+        return settings.FAILURE;
     }
+    return settings.SUCCESS;
 }
 
 function login(request, response, next){
@@ -61,7 +66,9 @@ function login(request, response, next){
         }
     } catch (e) {
         response.status(500).send(e.message);
+        return settings.FAILURE;
     }
+    return settings.SUCCESS;
 }
 
 function get_all_todo_items(request, response, next){
@@ -83,4 +90,79 @@ function get_all_todo_items(request, response, next){
     }
 }
 
+function add_item_to_todo_items(request, response, next){
+    var session_id,
+        task_id,
+        task_value,
+        user_name;
+    try {
+        user_name = request.cookies['user_name'];
+        session_id = request.cookies['sessionId'];
+        task_id = request.params['id'];
+        task_value = request.params['title'];
+        if ((user_name === undefined) || (session_id === undefined)
+            || (task_id === undefined) || (task_value === undefined)){
+            throw settings.bad_request_format_error;
+        }
+        if (users.check_user_valid(user_name, session_id)){
+            todoitems.add_item_to_user(user_name, task_id, task_value);
+            response.status(200).send(settings.STATUS_PHRASES[200]);
+        }
+    } catch (e) {
+        response.status(500).send(e.message);
+        return settings.FAILURE;
+    }
+    return settings.SUCCESS;
+}
 
+function update_item_in_todo_items(request, response, next){
+    var session_id,
+        task_id,
+        task_value,
+        task_status,
+        user_name;
+    try {
+        user_name = request.cookies['user_name'];
+        session_id = request.cookies['sessionId'];
+        task_id = request.params['id'];
+        task_value = request.params['title'];
+        task_status = request.params['completed'];
+        if ((user_name === undefined) || (session_id === undefined) || (task_id === undefined)){
+            throw settings.bad_request_format_error;
+        }
+        if (users.check_user_valid(user_name, session_id)){
+            todoitems.update_item_for_user(user_name, task_id, task_value, task_status);
+            response.status(200).send(settings.STATUS_PHRASES[200]);
+        }
+    } catch (e) {
+        response.status(500).send(e.message);
+        return settings.FAILURE;
+    }
+    return settings.SUCCESS;
+}
+
+function delete_item_in_todo_items(request, response, next){
+    var session_id,
+        task_id,
+        user_name;
+    try {
+        user_name = request.cookies['user_name'];
+        session_id = request.cookies['sessionId'];
+        task_id = request.params['id'];
+        if ((user_name === undefined) || (session_id === undefined) || (task_id === undefined)){
+            throw settings.bad_request_format_error;
+        }
+        if (users.check_user_valid(user_name, session_id)){
+            if (task_id == settings.DELETE_ALL){
+                todoitems.delete_all_items_for_user(user_name);
+            } else {
+                todoitems.delete_item_for_user(user_name, task_id);
+            }
+            response.status(200).send(settings.STATUS_PHRASES[200]);
+        }
+    } catch (e) {
+        response.status(500).send(e.message);
+        return settings.FAILURE;
+    }
+    return settings.SUCCESS;
+}
--- settings.js
+++ settings.js
@@ -20,3 +20,6 @@ exports.STATUS_PHRASES = {200: "OK",
                           500: "Parsing error",
                           404: "Not found"};
 exports.DEFALUT_TIME_TO_EXPIRE = 1000000;
+exports.DELETE_ALL = -1;
+exports.SUCCESS = 0;
+exports.FAILURE = 1;
