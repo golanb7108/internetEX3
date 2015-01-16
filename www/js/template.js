7298117f92b55684c54d7b8ef8eec8c364a90136
golanb7108 <golan3873@gmail.com>
2015-01-16 12:16:44 +0200

    start setting delete

--- www/js/template.js
+++ www/js/template.js
@@ -13,7 +13,7 @@
 		+		'<div class="view">'
 		+			'<input class="toggle" type="checkbox" {{checked}}>'
 		+			'<label>{{title}}</label>'
-		+			'<button class="destroy"></button>'
+		+			'<button class="destroy" onclick="deleteItem()"></button>'
 		+		'</div>'
 		+	'</li>';
 	}
