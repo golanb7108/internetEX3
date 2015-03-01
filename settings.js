52fc2f2ae358b9b66d81ef32d9edcd47ad3e9ffb
Amit Abel <amitabel1@gmail.com>
2015-03-01 19:45:01 +0200

    change time to expire

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ exports.METHODS_OPTIONS = ["GET","POST","PUT","DELETE","TRACE","OPTIONS", "CONNE
 exports.STATUS_PHRASES = {200: "OK",
                           500: "Internal Server Error",
                           404: "Not found"};
-exports.DEFALUT_TIME_TO_EXPIRE = 1000000;
+exports.DEFALUT_TIME_TO_EXPIRE = 0.5; // Maximum time in hours before asked to login again
 exports.DELETE_ALL = -1;
 exports.SUCCESS = 0;
 exports.FAILURE = 1;
